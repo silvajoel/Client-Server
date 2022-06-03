eb7e3002a8728778194a886eda21c921b5b5684e
silvajoel <silvajoel06@gmail.com>
2022-06-03 20:31:13 -0300

    Alterei

--- client.py
+++ client.py
@@ -12,7 +12,7 @@ list  = sys.argv[3] + " " + sys.argv[4]
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 
-s.sendall(list.encode())
+s.sendall(list.encode()) fdsfdsfsdf 
 
 nova_palavra = s.recv(1024)
 nova_palavra = nova_palavra.decode()
